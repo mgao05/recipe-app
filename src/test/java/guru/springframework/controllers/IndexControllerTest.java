b611e4d5a3f26c77ea706a13b0a57733740b8a0d
Mengran <mengrangao@gwu.edu>
2020-06-15 11:38:04 -0400

    Testing.

--- src/test/java/guru/springframework/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/controllers/IndexControllerTest.java
@@ -2,23 +2,28 @@ package guru.springframework.controllers;
 
 import guru.springframework.Domain.Recipe;
 import guru.springframework.services.RecipeServiceImpl;
-import org.aspectj.weaver.patterns.IVerificationRequired;
 import org.junit.Before;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.*;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.anySet;
 import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 class IndexControllerTest {
 
@@ -36,13 +41,26 @@ class IndexControllerTest {
     }
 
 
+
+    @Test
+    public void testMockMVC() throws Exception{
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("index"));
+    }
+
     @Test
     void getIndexPage() throws Exception{
 
         //given
         Set<Recipe> recipes = new HashSet<>();
         recipes.add(new Recipe());
-        recipes.add(new Recipe());
+
+        Recipe recipe = new Recipe();
+        recipe.setId(1l);
+
+        recipes.add(recipe);
 
         when(recipeService.getRecipies()).thenReturn(recipes);
 
